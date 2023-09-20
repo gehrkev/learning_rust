4969f1d27420a12ebf740c93d3cbe79ab8cd5798
gehrkev <54125592+gehrkev@users.noreply.github.com>
2023-09-20 16:04:23 -0300

    Section 7: Cargo, Crates, and Modules

---
+++ section_7/todo/src/lib.rs
@@ -0,0 +1,27 @@
+mod list {
+
+    pub struct Tasks{
+        pub item: String,
+    }
+
+    //cargo install cargo-modules <----- this one proposed by the instructor doesn't work
+    //^^^ error[E0063]: missing field `diagnostic_provider` in initializer of `ServerCapabilities`
+    //cargo install --locked cargo-modules <------- this one worked
+    //cargo modules generate tree
+    //cargo modules generate tree --with-types <--- doesn't work
+    //cargo modules generate tree --fns --types <--- use this instead
+
+}
+
+mod things_todo;
+use crate::things_todo::add_activity;
+use things_todo::items_completed;
+use things_todo::items_completed::test::test_fn;
+
+
+fn lets_add_task(){
+    let task = list::Tasks {item: String::from("Tasks")};
+    add_activity(); //relative path
+    items_completed::remove_task();
+    test_fn();
+}
