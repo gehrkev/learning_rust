6e2c2570a510d25d6fe2ee8dabe53235fbda1d3b
gehrkev <54125592+gehrkev@users.noreply.github.com>
2023-09-21 17:17:22 -0300

    Section 9: Error Handling

---
+++ section_9/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::ErrorKind;
+use std::fs::rename;
+use std::io::Error;
+
+fn main() {
+    // panic!("panicked here");
+
+    // let vec = vec![1];
+    // vec[10]; //panic! // $env:RUST_BACKTRACE=1
+    // let file = File::open("error.txt").expect("Error opening the file!");
+    // let file = match file {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("error2.txt") {
+    //             Ok(file_created) => file_created,
+    //             Err(err) => panic!("Cannot create the file!")
+    //         }
+    //         _ => panic!("Other error kind")
+    //     }
+    // };
+    rename_file().unwrap();
+}
+
+fn open_file() -> Result<File, Error> {
+    let file = File::open("error.txt")?;
+    Ok(file)
+}
+
+fn rename_file() -> Result<(), Error> {
+    let file = rename("error.txt", "renamed.txt")?;
+    Ok(file)
+}
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
